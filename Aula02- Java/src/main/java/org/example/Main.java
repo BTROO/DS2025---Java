7ddc0cad3152bddf2ad5b9c46efdc5aaa086da81
BTROO <kailane06brito@gmail.com>
2025-08-08 21:36:31 -0300

    Add files via upload

---
+++ Aula02- Java/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Numeros pares entre 100 a 120");
+        for(int i = 100; i <= 120; i += 2 ) {
+            System.out.println(i);
+        }
+
+
+    }
+}

7bf8f526ff13694e5fe1e6e39cd94b71af4f917a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-20 21:24:07 -0300

    Feat: + Classe Cliente

--- Encapsulamento/src/main/java/org/example/Atividade2/Loja.java
+++ Encapsulamento/src/main/java/org/example/Atividade2/Loja.java
@@ -4,10 +4,13 @@ public class Loja {
     public static void main(String[] args) {
         Livro lv1 = new Livro("Trono de Vidro", "Sarah J Mass", "25546878552155", 560, 59.99);
         Veiculo vl1 = new Veiculo("jh58f450", "Cinza", 5, 56, 270, 40);
+        Cliente cl1 = new Cliente("Claudio", 21, "256984563256","7a Travessa Manoelito Teixeira", "71985566223");
 
         System.out.println(lv1);
         System.out.println();
         System.out.println(vl1);
+        System.out.println();
+        System.out.println(cl1);
 
     }
 }
